f733062247d65c5786a4fb5dd8fafb65465ac055
Natasha <1016761@student.sae.edu.au>
2020-03-02 11:52:21 +1100

    SceneTransitioningStarted

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -4,11 +4,7 @@ using UnityEngine;
 
 public class SceneTransition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Animator animator;
 
     // Update is called once per frame
     void Update()
