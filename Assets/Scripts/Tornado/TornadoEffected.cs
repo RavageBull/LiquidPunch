dd07716e897ab2ce188b6f20a31478d2156ea0c5
Blaide <1017007@student.sae.edu.au>
2020-03-10 16:57:44 +1100

    Programming test scene Added, And  scripts for tornado stuff started

---
+++ Assets/Scripts/Tornado/TornadoEffected.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// THese are just Identifier scripts, may not end up using them if all rigidbodies should be effected by the tornado.
+/// </summary>
+public class TornadoEffected : MonoBehaviour
+{
+}
---
+++ Assets/Scripts/Tornado/TornadoPhysicsSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// Just throws objects around the tornado and gradually pulls them up into the air.
+/// </summary>
+public class TornadoPhysicsSystem : MonoBehaviour
+{
+    
+    public List<TornadoEffected> tornadoEffecteds;
+
+    public List<TornadoEffected> GetTornadoEffecteds()
+    {
+        return FindObjectsOfType<TornadoEffected>().ToList();
+    }
+    public Vector3 CalculateForceAroundAxis(TornadoEffected t)
+    {
+        return t.transform.position;
+    }
+
+}
