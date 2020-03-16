83c37ea9172b1f43bee99e19ffa32ff5ebf6f985
Blaide <1017007@student.sae.edu.au>
2020-03-17 00:02:52 +1100

    Tornado physics, And Boned Jellyfish model,
    Its seems silly to have bones in a jellyfish, because they don't have bones, but it may be the simplest and lightest weight way processing wise, to do it.

--- Assets/Scripts/Tornado/TornadoPhysicsSystem.cs
+++ Assets/Scripts/Tornado/TornadoPhysicsSystem.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -8,16 +9,87 @@ using UnityEngine;
 /// </summary>
 public class TornadoPhysicsSystem : MonoBehaviour
 {
-    
+    public float pullSpeed,rotationSpeed,heightOffset;
+    private Vector3 offsetPossition;
+    public AnimationCurve RotationDistanceModifierCurve;
+    public AnimationCurve PullDistanceModifierCurve;
+    private CapsuleCollider capsuleCollider;
     public List<TornadoEffected> tornadoEffecteds;
+    
+    private void Start()
+    {
+       // tornadoEffecteds = GetAllTornadoEffecteds();
+       capsuleCollider.GetComponent<CapsuleCollider>();
+       
+    }
 
-    public List<TornadoEffected> GetTornadoEffecteds()
+    private void FixedUpdate()
+    {
+        offsetPossition = transform.position + Vector3.up * heightOffset;
+        foreach (TornadoEffected t in tornadoEffecteds)
+        {
+            Rigidbody rB = t.GetComponent<Rigidbody>();
+            
+            
+          //  float distanceFromTornado = Vector3.Distance(rB.transform.position,transform.position);
+           // float distancePercentofRadius = distanceFromTornado/capsuleCollider.radius;
+            
+           // float offsetRotationSpeed = rotationSpeed * RotationDistanceModifierCurve.Evaluate(distancePercentofRadius);
+           // float offsetPullSpeed = pullSpeed * PullDistanceModifierCurve.Evaluate(distancePercentofRadius);
+            
+            
+            
+            //TODO maybe convert this to all AddForce and AddTorque. or Add Accelleration and Deceleration Artifically.
+            
+            Quaternion q = Quaternion.AngleAxis (rotationSpeed, transform.up);
+            rB.MovePosition (q * (rB.transform.position - offsetPossition) + offsetPossition);
+            rB.MoveRotation (rB.transform.rotation * q); 
+            rB.AddForce((offsetPossition - rB.transform.position) * pullSpeed * rB.mass);
+            //rB.MoveRotation();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<TornadoEffected>() != null)
+        {
+            tornadoEffecteds.Add(other.GetComponent<TornadoEffected>());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<TornadoEffected>() != null && tornadoEffecteds.Contains(other.GetComponent<TornadoEffected>()))
+        {
+            tornadoEffecteds.Remove(other.GetComponent<TornadoEffected>());
+        }
+    }
+
+    public List<TornadoEffected> GetAllTornadoEffecteds()
     {
         return FindObjectsOfType<TornadoEffected>().ToList();
     }
     public Vector3 CalculateForceAroundAxis(TornadoEffected t)
     {
+        Vector3 torque = new Vector3();
         return t.transform.position;
     }
 
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position + Vector3.up * heightOffset, 0.5f);
+
+        foreach (TornadoEffected t in tornadoEffecteds)
+        {
+            Rigidbody rB = t.GetComponent<Rigidbody>();
+            if (rB != null)
+            {
+                Gizmos.DrawRay(t.transform.position,rB.velocity);
+            }
+
+        }
+        
+        
+    }
 }
