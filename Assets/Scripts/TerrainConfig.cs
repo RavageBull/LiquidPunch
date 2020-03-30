69bb2ea07c73c0b91f8ffd5bd6e9ced24f9dcb96
Bryce <1013537@student.sae.edu.au>
2020-03-31 10:29:45 +1100

    Reflection Probe & Tree Script
    Changed the reflection probe for the water to look nicer
    Created a tree distance script that lets us change the render distance of trees

---
+++ Assets/Scripts/TerrainConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainConfig : MonoBehaviour
+{
+    void Start()
+    {
+        Terrain.activeTerrain.treeDistance = 10000;
+    }
+
+}
