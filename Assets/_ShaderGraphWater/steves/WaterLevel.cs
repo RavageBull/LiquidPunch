8b7941dcd67a859ab6ffe69e6e45c7a2be601817
Stephen Greenland <1013573@student.sae.edu.au>
2020-04-05 17:05:30 +1000

    water looking stormy

--- Assets/_ShaderGraphWater/steves/WaterLevel.cs
+++ Assets/_ShaderGraphWater/steves/WaterLevel.cs
@@ -5,17 +5,29 @@ using UnityEngine;
 public class WaterLevel : MonoBehaviour
 {
 
-    public Material Water;
+    public Material water;
+    private float timer;
+    public float risetime;
+    public float multipler;
     
     // Start is called before the first frame update
     void Start()
     {
+        timer = 0;
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
+
+        if (timer < risetime)
+        {
+            water.SetFloat("_WaveHeight",timer/multipler);
+            
+        }
         
     }
 }
