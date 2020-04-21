2a9b2faa34625d725f795510491f5eb776f4b4a3
Blaide <1017007@student.sae.edu.au>
2020-04-21 13:54:28 +1000

    mushroom Fades

--- Assets/Scripts/MushroomEmmissiveFade.cs
+++ Assets/Scripts/MushroomEmmissiveFade.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 public class MushroomEmmissiveFade : MonoBehaviour
 {
+
+    public float timer = 0;
+
+    public float fadeTime;
+    private Color startColor;
+    public bool fading = false;
+
+    private Color currentColor;
+
+    public Renderer emissiveRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        emissiveRenderer = GetComponent<Renderer>();
+        startColor = emissiveRenderer.material.GetColor("Color_ECD9926D");
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if (fading)
+        {
+            if (timer <= fadeTime)
+            {
+                currentColor = Color.Lerp(startColor, Color.black, timer / fadeTime);
+                timer += Time.deltaTime;
+            }
+            emissiveRenderer.material.SetColor("Color_ECD9926D", currentColor);
+        }
         
+
     }
     private void Awake()
     {
@@ -21,9 +42,10 @@ public class MushroomEmmissiveFade : MonoBehaviour
 
         SceneTransition.MushroomFadeEvent += FadeOut;
     }
-
     public void FadeOut(float timeSpan)
     {
-        Debug.Log("mushrooms should fade here when this is finished");
+        fading = true;
+        timer = 0;
+        fadeTime = timeSpan;
     }
 }
